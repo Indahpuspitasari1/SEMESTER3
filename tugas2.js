59fa6b90e5872d4c2690344adfead7ceaf2d92ba
Indahpuspitasari1 <114733787+Indahpuspitasari1@users.noreply.github.com>
2023-10-03 22:07:02 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,10 @@
+var firstName = "INDAH";
+var lastName = "PUSPITASARI";
+var age = 20;
+var isMarried = false;
+
+console.log(`Nama Depan: ${firstName}`);
+console.log(`Nama Belakang: ${lastName}`);
+console.log(`Nama lengkap : ${firstName} ${lastName}`);
+console.log(`Umur: ${age}`);
+console.log(`Status Pernikahan: ${isMarried}`);
