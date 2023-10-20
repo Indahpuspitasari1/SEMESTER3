4823d77bc985d270910715477b37cbd642a4f8e9
Indahpuspitasari1 <114733787+Indahpuspitasari1@users.noreply.github.com>
2023-10-20 21:32:16 +0700

    Add files via upload

---
+++ sesi3/array.js
@@ -0,0 +1,17 @@
+// Bertipe array
+
+const restaurant = [
+  {
+    name: "moana",
+    city: "ujung genteng",
+    favoriteDrink: "matcha drink",
+    favoriteFood: "grilled fish",
+    isVegan: true,
+  },
+];
+
+console.log("Nama : " + restaurant[0].name);
+console.log("Kota : " + restaurant[0].city);
+console.log("Minuman Favorit : " + restaurant[0].favoriteDrink);
+console.log("Makanan Favorit : " + restaurant[0].favoriteFood);
+console.log("Vegan : " + restaurant[0].isVegan);
---
+++ sesi3/object.js
@@ -0,0 +1,16 @@
+// Bertipe Object
+
+const restaurant = {
+  name: "moana",
+  city: "ujung genteng",
+  favoriteDrink: "matcha drink",
+  favoriteFood: "grilled fish",
+  isVegan: false,
+};
+
+const { name, city, favoriteDrink, favoriteFood, isVegan } = restaurant;
+console.log("Nama : " + name);
+console.log("Kota : " + city);
+console.log("Minuman Favorit : " + favoriteDrink);
+console.log("Makanan Favorit : " + favoriteFood);
+console.log("Vegan : " + isVegan);
